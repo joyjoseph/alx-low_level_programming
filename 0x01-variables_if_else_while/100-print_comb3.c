96999f2a8449c8fb28f060384ad1c749c9fe56ce
joyjoseph <joyjoseph9027@gmail.com>
2022-10-17 05:39:30 -0700

    Program that prints all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  *main - main block
  *Return:0 success, non zero fail
@@ -6,7 +7,7 @@
 int main(void)
 {
 	int i, k = '0', j;
-	
+
 	for (i = '0'; i <= '9'; i++)
 	{
 		for (j = k; j <= '9'; j++)
